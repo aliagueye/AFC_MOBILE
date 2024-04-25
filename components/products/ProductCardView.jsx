125f30d26c50f02c6a783bea5a1062d72386ace1
gueye <aliounebadaraalia.gueye.etu@univ-lille.fr>
2024-04-25 19:50:07 +0200

    first commit

---
+++ components/products/ProductCardView.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ProductCardView() {
+  return (
+    <View>
+      <Text>ProductCardView</Text>
+     </View>
+  );
+}
