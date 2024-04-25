0f3d82b866d2e89896062d56f3f0f07c61c183f6
gueye <aliounebadaraalia.gueye.etu@univ-lille.fr>
2024-04-25 19:46:20 +0200

    fichier contenannt les importations des differents composant qu'va utiliser dans notre page d'acceuil

---
+++ components/index.js
@@ -0,0 +1,14 @@
+import Welcome from "./home/Welcome";
+import Carousel from "./home/Carousel";
+import Heading from "./home/heading";
+import ProductRow from "./products/ProductRow";
+import Temoignage from "./home/Temoignage";
+import Footer from "./home/Footer";
+export{
+    Welcome,
+    Carousel,
+    Heading,
+    ProductRow,
+    Temoignage,
+    Footer
+}
